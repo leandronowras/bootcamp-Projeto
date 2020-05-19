954b8dc0c5aebb055082b5643157778ea74557bd
Leandro Abreu <l.leandro_abreu@hotmail.com>
2020-05-19 04:20:53 -0300

    inicio do back-end finalizado

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,35 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
 const app = express();
 
 app.use(express.json()); // adicinar esse middleware na frente de todos os outros
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 app.listen(3333, () => {
     console.log('server started at port 3333');
